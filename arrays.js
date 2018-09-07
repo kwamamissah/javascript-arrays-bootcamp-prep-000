df69467f1bba147f027cceef5f6cf1ce59d10d42
kwamamissah@gmail.com <kwamamissah@gmail.com>
2018-09-07 20:44:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,3 +21,8 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, element) {
   return array[element];
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array, element) {
+  array.push(element);
+  return array;
+}
