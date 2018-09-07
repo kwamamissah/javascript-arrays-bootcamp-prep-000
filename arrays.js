9bd1ca08712b6caddda49d67dccc2fe7bd9e83a1
kwamamissah@gmail.com <kwamamissah@gmail.com>
2018-09-07 20:56:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,3 +35,7 @@ function destructivelyRemoveElementFromEndOfArray(array,element) {
   array.pop(element);
   return array;
 }
+
+function removeElementFromEndOfArray(array,element) {
+  array.slice(0,array.lenght - 1);
+}
